08f30e5774c9c79e4d52be746a13dd55d2d5b3fe
barbbecker <bbeckerdarosa@gmail.com>
2019-02-25 14:12:22 -0300

    finalizando training bank

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { HeaderComponent } from './header/header.component';
 import { ContentComponent } from './content/content.component';
 import { FooterComponent } from './footer/footer.component';
 import { CadastroClientesComponent } from './cadastro-clientes/cadastro-clientes.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HomeLogadaComponent } from './home-logada/home-logada.component';
 
 @NgModule({
   declarations: [
@@ -16,13 +18,15 @@ import { CadastroClientesComponent } from './cadastro-clientes/cadastro-clientes
     HeaderComponent,
     ContentComponent,
     FooterComponent,
-    CadastroClientesComponent
+    CadastroClientesComponent,
+    HomeLogadaComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatDialogModule
+    MatDialogModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/cadastro-clientes/cadastro-clientes.component.ts
+++ src/app/cadastro-clientes/cadastro-clientes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-cadastro-clientes',
@@ -6,10 +8,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cadastro-clientes.component.css']
 })
 export class CadastroClientesComponent implements OnInit {
-
-  constructor() { }
+  formCadastro;
+  valoresForm: object;
+  conversao;
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.formCadastro = this.fb.group({
+      nome: [''],
+      cpf: [''],
+      email: [''],
+      telefone: [''],
+      endereco: ['']
+    });
+    setTimeout(() => {
+      this.formCadastro.patchValue({
+        nome: 'Bárbara Becker',
+        cpf: '86895726034',
+        email: 'bbeckerdarosa@gmail.com',
+        telefone: '51996823457',
+        endereco: 'Rua Mali'
+      });
+    }, 2000);
+    console.log(this.valoresForm);
+    this.formCadastro.valueChanges.pipe(
+      debounceTime(1000))
+      .subscribe(res => {
+        console.log(res);
+        this.valoresForm = res;
+      });
+  }
+  cadastro() {
+    this.conversao = JSON.stringify(this.valoresForm);
+    console.log(this.conversao);
+    localStorage.setItem('cadastro', this.conversao);
   }
 
 }
